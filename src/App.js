333862620eed37ec276954db6398e2e0e6c2ec02
chiomadans1759 <oluchukwuokpala1759@gmail.com>
2020-12-24 13:28:03 +0100

    Commit

---
+++ src/App.js
@@ -0,0 +1,88 @@
+import React, { useEffect, useState, useCallback } from 'react';
+import Modal from './components/Modal';
+import ImageCard from './components/ImageCard';
+import ImageModal from './components/ImageModal';
+import Header from './components/Header';
+import useDebounce from './components/hooks/Debounce';
+import axios from './utils/axios';
+import './App.css';
+
+function App() {
+  const [photos, setPhotos] = useState([]);
+  const [searchKeyWord, setSearchKeyWord] = useState('');
+  const [showModal, setShowModal] = useState(false);
+  const [selectedImage, setSelectedImage] = useState({});
+  const [loading, setLoading] = useState(false);
+  const [errorMessge, setErrorMessage] = useState('');
+
+  const debouncedSearchTerm = useDebounce(searchKeyWord, 500);
+
+  const toggleModal = () => {
+    setShowModal(!showModal);
+  };
+
+  const handleSelectedImage = (image) => {
+    setSelectedImage(image);
+    toggleModal();
+  };
+
+  const handleSearch = useCallback(async () => {
+    try {
+      setLoading(true);
+      setErrorMessage('');
+      const newPhotos = await axios.get(`?query=${debouncedSearchTerm}`);
+      setPhotos(newPhotos.data);
+    } catch (error) {
+      setErrorMessage('an error occured');
+    }
+    setLoading(false);
+  }, [debouncedSearchTerm]);
+
+  useEffect(() => {
+    if (debouncedSearchTerm) {
+      handleSearch();
+    }
+  }, [debouncedSearchTerm, handleSearch]);
+
+  useEffect(() => {
+    handleSearch();
+  }, [handleSearch]);
+
+  return (
+    <>
+      <div className='app'>
+        <Header handleSearch={setSearchKeyWord} />
+        <div className='images__container'>
+          <div className='images__wrapper'>
+            {photos.map((image) => (
+              <ImageCard
+                key={image.urls.regular}
+                image={image.urls.regular}
+                name={image.user.name}
+                location={image.user.location}
+                handleClick={() => handleSelectedImage(image)}
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+      {showModal ? (
+        <Modal>
+          <span className='cancel__button' onClick={toggleModal}>
+            &times;
+          </span>
+          <ImageModal
+            image={selectedImage.urls.regular}
+            name={selectedImage.user.name}
+            location={selectedImage.user.location}
+            detailClassName='modal__card__details'
+            nameClassName='name__modal'
+            locationClassName='location__modal'
+          />
+        </Modal>
+      ) : null}
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/hooks/Debounce.jsx
@@ -0,0 +1,18 @@
+import { useEffect, useState } from 'react';
+
+const Debounce = (value, delay) => {
+  // state and setters for debounced value
+  const [debouncedValue, setDebouncedValue] = useState(value);
+  useEffect(() => {
+    // set debounced value passed in after some specific delay
+    const handler = setTimeout(() => {
+      setDebouncedValue(value);
+    }, delay);
+    return () => {
+      clearTimeout(handler);
+    };
+  }, [value, delay]);
+  return debouncedValue;
+};
+
+export default Debounce;
